bef5f0f18e7acd1ef2ad7acfd6bc4c0bc976455d
ageanelevimdrd <madridageanelevi@gmail.com>
2020-02-15 01:30:36 +0800

    5x5 Matrix

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,339 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.A1 = New System.Windows.Forms.Label()
+        Me.A2 = New System.Windows.Forms.Label()
+        Me.A3 = New System.Windows.Forms.Label()
+        Me.A4 = New System.Windows.Forms.Label()
+        Me.A5 = New System.Windows.Forms.Label()
+        Me.A6 = New System.Windows.Forms.Label()
+        Me.A7 = New System.Windows.Forms.Label()
+        Me.A8 = New System.Windows.Forms.Label()
+        Me.A9 = New System.Windows.Forms.Label()
+        Me.A10 = New System.Windows.Forms.Label()
+        Me.A11 = New System.Windows.Forms.Label()
+        Me.A12 = New System.Windows.Forms.Label()
+        Me.A13 = New System.Windows.Forms.Label()
+        Me.A14 = New System.Windows.Forms.Label()
+        Me.A15 = New System.Windows.Forms.Label()
+        Me.A16 = New System.Windows.Forms.Label()
+        Me.A17 = New System.Windows.Forms.Label()
+        Me.A18 = New System.Windows.Forms.Label()
+        Me.A19 = New System.Windows.Forms.Label()
+        Me.A20 = New System.Windows.Forms.Label()
+        Me.A21 = New System.Windows.Forms.Label()
+        Me.A22 = New System.Windows.Forms.Label()
+        Me.A23 = New System.Windows.Forms.Label()
+        Me.A24 = New System.Windows.Forms.Label()
+        Me.A25 = New System.Windows.Forms.Label()
+        Me.Label25 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'A1
+        '
+        Me.A1.BackColor = System.Drawing.Color.DimGray
+        Me.A1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A1.Location = New System.Drawing.Point(12, 19)
+        Me.A1.Name = "A1"
+        Me.A1.Size = New System.Drawing.Size(73, 51)
+        Me.A1.TabIndex = 1
+        '
+        'A2
+        '
+        Me.A2.BackColor = System.Drawing.Color.DimGray
+        Me.A2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A2.Location = New System.Drawing.Point(101, 19)
+        Me.A2.Name = "A2"
+        Me.A2.Size = New System.Drawing.Size(73, 51)
+        Me.A2.TabIndex = 2
+        '
+        'A3
+        '
+        Me.A3.BackColor = System.Drawing.Color.DimGray
+        Me.A3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A3.Location = New System.Drawing.Point(190, 19)
+        Me.A3.Name = "A3"
+        Me.A3.Size = New System.Drawing.Size(73, 51)
+        Me.A3.TabIndex = 3
+        '
+        'A4
+        '
+        Me.A4.BackColor = System.Drawing.Color.DimGray
+        Me.A4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A4.Location = New System.Drawing.Point(278, 19)
+        Me.A4.Name = "A4"
+        Me.A4.Size = New System.Drawing.Size(73, 51)
+        Me.A4.TabIndex = 4
+        '
+        'A5
+        '
+        Me.A5.BackColor = System.Drawing.Color.DimGray
+        Me.A5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A5.Location = New System.Drawing.Point(366, 19)
+        Me.A5.Name = "A5"
+        Me.A5.Size = New System.Drawing.Size(73, 51)
+        Me.A5.TabIndex = 5
+        '
+        'A6
+        '
+        Me.A6.BackColor = System.Drawing.Color.DimGray
+        Me.A6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A6.Location = New System.Drawing.Point(12, 82)
+        Me.A6.Name = "A6"
+        Me.A6.Size = New System.Drawing.Size(73, 51)
+        Me.A6.TabIndex = 6
+        '
+        'A7
+        '
+        Me.A7.BackColor = System.Drawing.Color.DimGray
+        Me.A7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A7.Location = New System.Drawing.Point(101, 82)
+        Me.A7.Name = "A7"
+        Me.A7.Size = New System.Drawing.Size(73, 51)
+        Me.A7.TabIndex = 7
+        '
+        'A8
+        '
+        Me.A8.BackColor = System.Drawing.Color.DimGray
+        Me.A8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A8.Location = New System.Drawing.Point(190, 82)
+        Me.A8.Name = "A8"
+        Me.A8.Size = New System.Drawing.Size(73, 51)
+        Me.A8.TabIndex = 8
+        '
+        'A9
+        '
+        Me.A9.BackColor = System.Drawing.Color.DimGray
+        Me.A9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A9.Location = New System.Drawing.Point(278, 82)
+        Me.A9.Name = "A9"
+        Me.A9.Size = New System.Drawing.Size(73, 51)
+        Me.A9.TabIndex = 9
+        '
+        'A10
+        '
+        Me.A10.BackColor = System.Drawing.Color.DimGray
+        Me.A10.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A10.Location = New System.Drawing.Point(366, 82)
+        Me.A10.Name = "A10"
+        Me.A10.Size = New System.Drawing.Size(73, 51)
+        Me.A10.TabIndex = 10
+        '
+        'A11
+        '
+        Me.A11.BackColor = System.Drawing.Color.DimGray
+        Me.A11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A11.Location = New System.Drawing.Point(12, 148)
+        Me.A11.Name = "A11"
+        Me.A11.Size = New System.Drawing.Size(73, 51)
+        Me.A11.TabIndex = 11
+        '
+        'A12
+        '
+        Me.A12.BackColor = System.Drawing.Color.DimGray
+        Me.A12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A12.Location = New System.Drawing.Point(101, 148)
+        Me.A12.Name = "A12"
+        Me.A12.Size = New System.Drawing.Size(73, 51)
+        Me.A12.TabIndex = 12
+        '
+        'A13
+        '
+        Me.A13.BackColor = System.Drawing.Color.DimGray
+        Me.A13.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A13.Location = New System.Drawing.Point(190, 148)
+        Me.A13.Name = "A13"
+        Me.A13.Size = New System.Drawing.Size(73, 51)
+        Me.A13.TabIndex = 13
+        '
+        'A14
+        '
+        Me.A14.BackColor = System.Drawing.Color.DimGray
+        Me.A14.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A14.Location = New System.Drawing.Point(278, 148)
+        Me.A14.Name = "A14"
+        Me.A14.Size = New System.Drawing.Size(73, 51)
+        Me.A14.TabIndex = 14
+        '
+        'A15
+        '
+        Me.A15.BackColor = System.Drawing.Color.DimGray
+        Me.A15.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A15.Location = New System.Drawing.Point(366, 148)
+        Me.A15.Name = "A15"
+        Me.A15.Size = New System.Drawing.Size(73, 51)
+        Me.A15.TabIndex = 15
+        '
+        'A16
+        '
+        Me.A16.BackColor = System.Drawing.Color.DimGray
+        Me.A16.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A16.Location = New System.Drawing.Point(12, 214)
+        Me.A16.Name = "A16"
+        Me.A16.Size = New System.Drawing.Size(73, 51)
+        Me.A16.TabIndex = 16
+        '
+        'A17
+        '
+        Me.A17.BackColor = System.Drawing.Color.DimGray
+        Me.A17.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A17.Location = New System.Drawing.Point(101, 214)
+        Me.A17.Name = "A17"
+        Me.A17.Size = New System.Drawing.Size(73, 51)
+        Me.A17.TabIndex = 17
+        '
+        'A18
+        '
+        Me.A18.BackColor = System.Drawing.Color.DimGray
+        Me.A18.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A18.Location = New System.Drawing.Point(190, 214)
+        Me.A18.Name = "A18"
+        Me.A18.Size = New System.Drawing.Size(73, 51)
+        Me.A18.TabIndex = 18
+        '
+        'A19
+        '
+        Me.A19.BackColor = System.Drawing.Color.DimGray
+        Me.A19.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A19.Location = New System.Drawing.Point(278, 214)
+        Me.A19.Name = "A19"
+        Me.A19.Size = New System.Drawing.Size(73, 51)
+        Me.A19.TabIndex = 19
+        '
+        'A20
+        '
+        Me.A20.BackColor = System.Drawing.Color.DimGray
+        Me.A20.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A20.Location = New System.Drawing.Point(366, 214)
+        Me.A20.Name = "A20"
+        Me.A20.Size = New System.Drawing.Size(73, 51)
+        Me.A20.TabIndex = 20
+        '
+        'A21
+        '
+        Me.A21.BackColor = System.Drawing.Color.DimGray
+        Me.A21.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A21.Location = New System.Drawing.Point(12, 279)
+        Me.A21.Name = "A21"
+        Me.A21.Size = New System.Drawing.Size(73, 51)
+        Me.A21.TabIndex = 21
+        '
+        'A22
+        '
+        Me.A22.BackColor = System.Drawing.Color.DimGray
+        Me.A22.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A22.Location = New System.Drawing.Point(101, 279)
+        Me.A22.Name = "A22"
+        Me.A22.Size = New System.Drawing.Size(73, 51)
+        Me.A22.TabIndex = 22
+        '
+        'A23
+        '
+        Me.A23.BackColor = System.Drawing.Color.DimGray
+        Me.A23.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A23.Location = New System.Drawing.Point(190, 279)
+        Me.A23.Name = "A23"
+        Me.A23.Size = New System.Drawing.Size(73, 51)
+        Me.A23.TabIndex = 23
+        '
+        'A24
+        '
+        Me.A24.BackColor = System.Drawing.Color.DimGray
+        Me.A24.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A24.Location = New System.Drawing.Point(278, 279)
+        Me.A24.Name = "A24"
+        Me.A24.Size = New System.Drawing.Size(73, 51)
+        Me.A24.TabIndex = 24
+        '
+        'A25
+        '
+        Me.A25.BackColor = System.Drawing.Color.DimGray
+        Me.A25.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.A25.Location = New System.Drawing.Point(366, 279)
+        Me.A25.Name = "A25"
+        Me.A25.Size = New System.Drawing.Size(73, 51)
+        Me.A25.TabIndex = 25
+        '
+        'Label25
+        '
+        Me.Label25.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label25.ForeColor = System.Drawing.Color.White
+        Me.Label25.Location = New System.Drawing.Point(117, 344)
+        Me.Label25.Name = "Label25"
+        Me.Label25.Size = New System.Drawing.Size(216, 59)
+        Me.Label25.TabIndex = 26
+        Me.Label25.Text = "Ageane Levi A. Madrid             BSCpE 5-3"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(455, 412)
+        Me.Controls.Add(Me.Label25)
+        Me.Controls.Add(Me.A25)
+        Me.Controls.Add(Me.A24)
+        Me.Controls.Add(Me.A23)
+        Me.Controls.Add(Me.A22)
+        Me.Controls.Add(Me.A21)
+        Me.Controls.Add(Me.A20)
+        Me.Controls.Add(Me.A19)
+        Me.Controls.Add(Me.A18)
+        Me.Controls.Add(Me.A17)
+        Me.Controls.Add(Me.A16)
+        Me.Controls.Add(Me.A15)
+        Me.Controls.Add(Me.A14)
+        Me.Controls.Add(Me.A13)
+        Me.Controls.Add(Me.A12)
+        Me.Controls.Add(Me.A11)
+        Me.Controls.Add(Me.A10)
+        Me.Controls.Add(Me.A9)
+        Me.Controls.Add(Me.A8)
+        Me.Controls.Add(Me.A7)
+        Me.Controls.Add(Me.A6)
+        Me.Controls.Add(Me.A5)
+        Me.Controls.Add(Me.A4)
+        Me.Controls.Add(Me.A3)
+        Me.Controls.Add(Me.A2)
+        Me.Controls.Add(Me.A1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "5x5 Matrix"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents A1 As Label
+    Friend WithEvents A2 As Label
+    Friend WithEvents A3 As Label
+    Friend WithEvents A4 As Label
+    Friend WithEvents A5 As Label
+    Friend WithEvents A6 As Label
+    Friend WithEvents A7 As Label
+    Friend WithEvents A8 As Label
+    Friend WithEvents A9 As Label
+    Friend WithEvents A10 As Label
+    Friend WithEvents A11 As Label
+    Friend WithEvents A12 As Label
+    Friend WithEvents A13 As Label
+    Friend WithEvents A14 As Label
+    Friend WithEvents A15 As Label
+    Friend WithEvents A16 As Label
+    Friend WithEvents A17 As Label
+    Friend WithEvents A18 As Label
+    Friend WithEvents A19 As Label
+    Friend WithEvents A20 As Label
+    Friend WithEvents A21 As Label
+    Friend WithEvents A22 As Label
+    Friend WithEvents A23 As Label
+    Friend WithEvents A24 As Label
+    Friend WithEvents A25 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Timer1 As Timer
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,112 @@
 ﻿Public Class Form1
+    Dim Aa, B, C, D, E As Boolean
 
+    Private time As Byte
+
+    Private binary As Byte
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds
+        Timer1.Start()
+    End Sub
+
+    Private Sub A_Click(sender As Object, e As EventArgs) Handles A1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles A2.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        binary = time
+
+        Aa = CBool(binary >= 8)
+        If Aa Then
+            binary -= 8
+        End If
+
+        B = CBool(binary >= 4)
+        If B Then
+            binary -= 4
+        End If
+
+        C = CBool(binary >= 2)
+        If C Then
+            binary -= 2
+        End If
+
+        D = CBool(binary >= 1)
+        If D Then
+            binary -= 1
+        End If
+
+        IsOn(CBool((C And (Not B Or D)) Or (B And Not C)), A1)
+
+        IsOn(CBool(C Or Aa Or Not (B Xor D)), A2)
+
+        IsOn(CBool(Not B Or D Or C), A3)
+
+        IsOn(CBool(C Or Aa Or Not (B Xor D)), A4)
+
+        IsOn(B, A5)
+
+        IsOn(CBool(Aa Or (Not C And Not D) Or (B And (Not C Or Not D))), A6)
+
+        IsOn(CBool(Not Aa And Not B And Not C And D), A7)
+
+        IsOn(CBool(Not Aa And Not B And Not C And D), A8)
+
+        IsOn(CBool(Not Aa And Not B And Not C And Not D), A9)
+
+        IsOn(CBool(Aa Or (Not B And Not D) Or Not (C Xor D)), A10)
+
+        IsOn(CBool((Not Aa And Not B And Not C And Not D) Or (B And (C Xor D))), A11)
+
+        IsOn(CBool(Aa Or (C And Not D) Or (B And Not C)), A12)
+
+        IsOn(CBool(Not B Or Not C Or Not D), A13)
+
+        IsOn(CBool(Aa Or B Or C), A14)
+
+        IsOn(CBool((Not Aa And Not C And Not D) Or (Aa And D)), A15)
+
+        IsOn(CBool(Not D And (Not B Or C)), A16)
+
+        IsOn(CBool(Not Aa And Not B And Not C And Not D), A17)
+
+        IsOn(CBool(Not Aa And Not B And Not C And D), A18)
+
+        IsOn(CBool(D And B And C), A19)
+
+        IsOn(CBool(Aa Or (Not C And Not D) Or (Not B And C And D) Or (B And (Not C Or Not D))), A20)
+
+        IsOn(CBool((Not B And C) Or (B And Not C And D)), A21)
+
+        IsOn(CBool((Not D And (Not B Or C)) Or (Not Aa And (Not B Or (Not C And D)))), A22)
+
+        IsOn(CBool(Not B Or (C Xor D)), A23)
+
+        IsOn(CBool(Not B Or C Or D), A24)
+
+        IsOn(CBool(Not D And (B Xor C)), A25)
+
+
+        If time = 9 Then
+            time = 0
+        Else
+            time += 1
+        End If
+    End Sub
+
+    Private Sub IsOn(x As Boolean, A As Label)
+        Console.WriteLine(x)
+        Console.WriteLine(A)
+
+        If x Then
+            A.BackColor = Color.Red
+        Else
+            A.BackColor = Color.DimGray
+        End If
+    End Sub
 End Class
